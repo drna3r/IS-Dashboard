93e054f5577bbd73997c46fa92d915886baac6f0
na3r <na3r1404@gmail.com>
2016-09-17 14:51:28 +0430

    final title
    its final title dont change it !

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
   <head>
   <!-- Test More HR... -->
     <meta charset="UTF-8">
-<<<<<<< HEAD
     <title>InfoSalamat Dashboard</title>
-=======
-    <title>afshin AdminLTE 2 | Dashboard</title>
->>>>>>> refs/remotes/origin/master
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.4 -->
